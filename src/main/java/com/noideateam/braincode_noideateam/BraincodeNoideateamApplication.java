c41f10c6e3e8651e04bc6b2ee9be170c67919d2a
Maciej S <starwars.maciek@gmail.com>
2019-04-12 21:21:35 +0200

    init 2

---
+++ src/main/java/com/noideateam/braincode_noideateam/BraincodeNoideateamApplication.java
@@ -0,0 +1,13 @@
+package com.noideateam.braincode_noideateam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BraincodeNoideateamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BraincodeNoideateamApplication.class, args);
+    }
+
+}
