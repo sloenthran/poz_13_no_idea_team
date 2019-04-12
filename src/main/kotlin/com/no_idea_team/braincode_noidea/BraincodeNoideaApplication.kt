745c4c34de74e2a2b846349d08437ca46ffd7c4f
Maciej S <starwars.maciek@gmail.com>
2019-04-12 20:12:50 +0200

    Init

---
+++ src/main/kotlin/com/no_idea_team/braincode_noidea/BraincodeNoideaApplication.kt
@@ -0,0 +1,11 @@
+package com.no_idea_team.braincode_noidea
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BraincodeNoideaApplication
+
+fun main(args: Array<String>) {
+    runApplication<BraincodeNoideaApplication>(*args)
+}
---
+++ src/test/kotlin/com/no_idea_team/braincode_noidea/BraincodeNoideaApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.no_idea_team.braincode_noidea
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class BraincodeNoideaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
